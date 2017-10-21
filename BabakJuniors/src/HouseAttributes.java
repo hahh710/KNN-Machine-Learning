73be1a07e7203dbbb1ec05af774044abbc32ea69
Ahmed Khattab <ahmed.khattab20@hotmail.com>
2017-10-21 00:58:50 -0400

    Interface Test
    Just testing out github and adding the interface to github

---
+++ BabakJuniors/src/HouseAttributes.java
@@ -0,0 +1,10 @@
+/**
+ * Write a description of class HouseAttributes here.
+ *
+ */
+public interface HouseAttributes
+{
+    // Method used to normalize the value of each house specification
+	public float getNormalizedValue();
+    
+}
