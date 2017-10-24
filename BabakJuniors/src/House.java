d918784a8251d8dd45b8cd92d974fc7329cab7a5
hahh710 <hunhoha@cmail.carleton.ca>
2017-10-24 11:20:47 -0400

    Update House.java

--- BabakJuniors/src/House.java
+++ BabakJuniors/src/House.java
@@ -9,22 +9,40 @@ import java.util.List;
  *
  */
 public class House {
-	private int price;
-	private List<HouseAttribute> houseAttributes;
+	private List<Object> houseAttributes = new ArrayList<Object>();
+	private int price ;
+	
 	public House(Coordinates c, Age a, SqrFt s, int p) {
-		price =p;
+		//index 0 of arraylist has coordinates
+		//index 1, Age
+		//index 2, sqrFt 
+		//index 3, price
+		houseAttributes.add(0,c);
+		houseAttributes.add(1,a);
+		houseAttributes.add(2,s);
+		houseAttributes.add(3,p);
+		price = p;
 	}
+	
 	public House(Coordinates c, Age a, SqrFt s) {//constructor for new house aka unknown price
+		houseAttributes.add(c);
+		houseAttributes.add(a);
+		houseAttributes.add(s);
 		price=0;
 	}
+	
 	public House() {
 		price=0;
 	}
+	
 	public int getPrice() {
 		return price;
 	}
+	
 	public void setPrice(int p) {
 		price=p;
 	}
-	public 
+	public List<Object> getHouse(){
+		return houseAttributes; 
+	}
 }
