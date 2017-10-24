d86f3740c2279a10f6d21b2beff854aa0171fcb1
Arsalan Sadiq <32456066+arsalansadiq@users.noreply.github.com>
2017-10-24 19:09:26 +0400

    New SquareFeet Class
    This class contains a getter method for squareFeet and an normalized method

---
+++ BabakJuniors/src/SquareFeet.java
@@ -0,0 +1,36 @@
+
+public class SquareFeet {
+	 // instance variables - replace the example below with your own
+    private int squareFeet;
+
+    /**
+     * Constructor for objects of class SquareFeet
+     */
+    public SquareFeet(int squareFeet)
+    {
+        // initialise instance variables
+        this.squareFeet=squareFeet;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int getsquarefeet(){
+    	return squareFeet;
+    }
+    
+    public float getNormalizedValue () {
+    	float value=(float) 0;
+    	if(this.squareFeet>2000 && this.squareFeet==2000){
+    		value = (float) 2.0;
+    	}else if(this.squareFeet>1000 && this.squareFeet<2000){
+    		value = (float) 1.5;
+    	}else{
+    		value = (float) 1.0;
+    	}
+    	return value;
+    }
+}
