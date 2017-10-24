f5924cfce022154042ef547565e004d5ba85b9f4
Arsalan Sadiq <32456066+arsalansadiq@users.noreply.github.com>
2017-10-24 20:40:06 +0400

    Added major changes
    added major changes on the method and added java doc

--- BabakJuniors/src/SqrFt.java
+++ BabakJuniors/src/SqrFt.java
@@ -1,36 +1,49 @@
 
+/**
+ *SqrFt
+ *
+ *@Authors: Babak Juniors 
+ *@Purpose: An attribute of a house which takes in an integer and using that it gets a normalized value which is under a certain limit
+ *
+ *@field variables
+ *	squareFeet: An integer value of squarefeet
+ *	
+ *@methods
+ *	main:
+ *	getNrmalizedValue: returns a float value which is under 0-1 depending on the squareFeet of an house.
+ *
+ */
 public class SqrFt {
-	 // instance variables - replace the example below with your own
-    private int squareFeet;
+	// instance variables - replace the example below with your own
+	private int squareFeet;
+	/**
+	 * Constructor for objects of class SquareFeet
+	 */
+	public SqrFt(int squareFeet)
+	{
+		// initialise instance variables
+		this.squareFeet=squareFeet;
+	}
 
-    /**
-     * Constructor for objects of class SquareFeet
-     */
-    public SqrFt(int squareFeet)
-    {
-        // initialise instance variables
-        this.squareFeet=squareFeet;
-    }
+	public int getSqrFt(){
+		return squareFeet;
+	}
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int getSqrFt(){
-    		return squareFeet;
-    }
-    
-    public float getNormalizedValue () {
-    	float value=(float) 0;
-    	if(this.squareFeet>2000 && this.squareFeet==2000){
-    		value = (float) 2.0;
-    	}else if(this.squareFeet>1000 && this.squareFeet<2000){
-    		value = (float) 1.5;
-    	}else{
-    		value = (float) 1.0;
-    	}
-    	return value;
-    }
+	public float getNormalizedValue () {
+		float value = this.squareFeet;
+
+		return (float) (value / (10000));
+
+		//if(this.squareFeet>2000 || this.squareFeet==2000){
+		//value = (float) 2.0;
+		//}else if(this.squareFeet>1000 || this.squareFeet<2000){
+		//value = (float) 1.5;
+		//}else{
+		//value = (float) 1.0;
+		//}
+		//return value;
+
+	}
+
+
 }
