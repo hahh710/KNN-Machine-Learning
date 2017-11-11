3ab8d1b86c8720204c7e4357ddda3cd6e683f7ca
JTudor <Jeff.A.Tudor@gmail.com>
2017-11-11 18:59:28 -0500

    Refactored knn, decoupled and put distance in its own class
    knn and distances

--- BabakJuniors/src/Distance.java
+++ BabakJuniors/src/Distance.java
@@ -1,9 +1,100 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Distance {
-
+//for each feature in test there is a distance between that and 
+	private Map<String,ArrayList<Float>> distances;
+	private Map<String,ArrayList<Float>> normDist;
+	private Map<TrainingExample, Integer> lookUpTable;
+	private TestingExample testEx;
 	public Distance(TestingExample testingExample, ArrayList<TrainingExample> trainingExamples) {
-		// TODO Auto-generated constructor stub
+		testEx= testingExample;
+		distances = new HashMap<String, ArrayList<Float>>();
+		normDist = new HashMap<String, ArrayList<Float>>();
+		lookUpTable = new HashMap<TrainingExample, Integer>();
+		
 	}
-
+	public Float getDistance(Feature train, Feature test){
+		if(test.getCorX()!=null){
+			return getEuclideanDistance(train, test);
+		}
+		if(test.getStringValue)!=null){
+			return getSubjectiveDistance(train, test);
+		}
+		if(test.getNumValue()!=null){
+			return getAbsoluteDistance(train, test);
+		}
+		return null;
+	}
+	/**
+	 * Get position in array list of training example
+	 * Go to each entry and add up the distances at that position
+	 * How to guarantee that features of a training example will align
+	 * 
+	 * @param testExam
+	 * @param trainExam
+	 * @return
+	 */
+	public float findDistance(TestingExample testExam, TrainingExample trainExam) {
+		int index = lookUpTable.get(trainExam);
+		int sum=0;
+		for (Map.Entry<String, ArrayList<Float>> entry: normDist.entrySet()) {
+			sum+=entry.getValue().get(index);
+		}
+		return sum;
+	}
+	/**
+	 * 
+	 * This will find max distance in the list of distances for a feature, 
+	 * then it will divide each distance of that feature by the max
+	 */
+	public void normalizeDistance() {
+		//mi gna super sako
+		normDist.clear();
+		normDist.putAll(distances);
+		float maxDistance=0;
+		for (Map.Entry<String, ArrayList<Float>> entry: normDist.entrySet()) {
+			for(Float f:entry.getValue()) {//for each element in array in entry
+				if(f!=null) {
+					if(f>maxDistance) {
+						maxDistance=f;
+					}
+				}
+			}
+			for(Float f:entry.getValue()) {//for each element in array in entry
+				f=f/maxDistance;
+			}
+		}
+	}
+	/**
+	 * 
+	 */
+	public void updateDistances() {
+		distances.clear();
+		int index=0;
+		int lookUpTableFlag =0;
+		for (Feature f: testEx.getFeatures()) {
+			distances.put(f.getName(), new ArrayList<Float>());
+		}
+		for (Feature f: testEx.getFeatures()) {
+			for(TrainingExample t: testEx.getExample().getTrainingExamples()) {
+				if(lookUpTableFlag==0) {
+					lookUpTable.put(t, index);
+					index++;
+				}
+				distances.get(f.getFName()).add(getDistance(f, t.getFeature(f.getFName())));
+			}
+			lookUpTableFlag++;
+		}
+	}
+	public float getEuclideanDistance(Feature train, Feature test){
+		return (float) Math.sqrt(Math.pow(train.getCorX() - test.getCorX(), 2) + Math.pow(train.getCorY() - test.getCorY(), 2)));
+	}
+	public float getSubjectiveDistance(Feature train, Feature test){
+		return train.getRank()-test.getRank();
+	}
+	public float getAbsoluteDistance(Feature train, Feature test){
+		return train.getNumValue()-test.getNumValue();
+	}
 }
--- BabakJuniors/src/KNN.java
+++ BabakJuniors/src/KNN.java
@@ -14,25 +14,27 @@ import java.util.List;
  * 
  * @methods main: findKNN: findDistance: findPrice:
  *
+ *
+ *the KNN class will be called from a testing example to find its nearest neighbors
+ *
+ *finds neighbors of a test example from a list of training example
  */
 public class KNN {
-	private List<House> houses;
-	private List<House> nearestNeighbors;
-
-	public List<House> getNearestNeighbors() {
+	private ArrayList<TrainingExample> nearestNeighbors;
+	private int k;
+	private TestingExample testEx;
+	public KNN(int k, TestingExample testEx) {
+		this.k=k;
+		this.testEx=testEx;
+		nearestNeighbors = new ArrayList<TrainingExample>();
+	}
+	public List<TrainingExample> getNN() {
+		nearestNeighbors.clear();
+		findKNN(k, testEx, testEx.getExampleManager().getTrainingExamples());
 		return nearestNeighbors;
 	}
-
 	public void setNearestNeighbors(List<House> nearestNeighbors) {
 		this.nearestNeighbors = nearestNeighbors;
-	}
-
-	private House newHouse;
-
-	public KNN() {
-		houses = new ArrayList<House>();
-		nearestNeighbors = new ArrayList<House>();
-		newHouse = null;
 	}
 
 	/**
@@ -50,163 +52,36 @@ public class KNN {
 	 *            appended house 4. Repeat k times
 	 * 
 	 */
-	public void findKNN(int k, House newHouse, ArrayList<House> potentialNN) {
-		House nearestNeighbor = new House();
+	public void findKNN(int k, TestingExample testEx, ArrayList<TrainingExample> potentialNN) {
+		TrainingExample nearestNeighbor = new TrainingExample();
 		if (k > 0) { // if K>0 then must find nearestNeighbor and append to list
 						// of NN
 			float distance = -1;
 			float smallestDistance = -1;
-			for (House h : potentialNN) {
-				distance = findDistance(h, newHouse);
+			for (TrainingExample t : potentialNN) {
+				distance = testEx.getDistances().findDistance(t, testEx);
 				if (smallestDistance == -1) {// first house in list will
 												// initially be NN
 					smallestDistance = distance;
-					nearestNeighbor = h;
+					nearestNeighbor = t;
 				}
 				if (smallestDistance > distance) {// new smallest distance
 					smallestDistance = distance;
-					nearestNeighbor = h;
+					nearestNeighbor = t;
 				}
 			}
 			nearestNeighbors.add(nearestNeighbor);// append NN
-			ArrayList<House> newPotentialNN = new ArrayList<House>();// smelllllllly
-			for (House h : potentialNN) {
-				newPotentialNN.add(h);
+			ArrayList<TrainingExample> newPotentialNN = new ArrayList<TrainingExample>();
+			for (TrainingExample t : potentialNN) {
+				newPotentialNN.add(t);
 			}
 			newPotentialNN.remove(nearestNeighbor);// remove the determined
 													// nearestNeighbor
-			findKNN(k - 1, newHouse, newPotentialNN);// Recursively call until k
+			findKNN(k - 1, testEx, newPotentialNN);// Recursively call until k
 														// neighbors have been
 														// determined
 		}
 
 		// K nearest neighbors have been determined
 	}
-
-	/**
-	 * findPrice(List<House> NN)
-	 * 
-	 * @Parameters -list of houses -> nearest neighbors determined by findKNN
-	 *             method
-	 *
-	 * @returns -returns price of new house -> simply calculated by averaging
-	 *          nearest neighbors for the purposes of milestone 1
-	 *
-	 */
-	public int findPrice() {
-		float sum = 0;
-		int count = 0;
-		for (House h : nearestNeighbors) {
-			sum += h.getPrice();
-			count++;
-		}
-		return (int) sum / count;
-	}
-
-	/**
-	 * findDistance (House h1, House h2)
-	 * 
-	 * @param -two
-	 *            houses that are being compared
-	 * @return -return a float representing the distance between the two houses
-	 * 
-	 *         Determine distance between two houses //index 0 of arraylist has
-	 *         coordinates //index 1, Age //index 2, sqrFt //index 3, price
-	 */
-	public float findDistance(House h1, House h2) {
-		return findCoordinateDistance(h1, h2) + findAgeDistance(h1, h2) + findSqrFtDistance(h1, h2);
-	}
-
-	/**
-	 * findCoordinateDistance(House h1, House h2)
-	 * 
-	 * @param -two
-	 *            houses that are being compared
-	 * @return -returns a float representing the euclidean distance between the
-	 *         two houses which are being compared
-	 * 
-	 *         Determine the euclidean distance between two houses using
-	 *         dist(h1(x, y), h2(a, b)) = sqrt((x - a)� + (y - b)�)
-	 */
-	public float findCoordinateDistance(House h1, House h2) {
-		return Math.abs(
-				(float) Math.sqrt(Math.pow(h1.getCorX() - h2.getCorX(), 2) + Math.pow(h1.getCorY() - h2.getCorY(), 2)));
-	}
-
-	/**
-	 * Determines the age distance
-	 * 
-	 * @param house
-	 *            h1 and house h2
-	 * @returns the age distance between the two houses
-	 */
-	public float findAgeDistance(House h1, House h2) {
-		return Math.abs(h1.getAge() - h2.getAge());
-	}
-
-	/**
-	 * Determines the sqrft distance
-	 * 
-	 * @param house
-	 *            h1 and house h2
-	 * @returns the sqrft distance between the two houses
-	 */
-	public float findSqrFtDistance(House h1, House h2) {
-		return Math.abs(h1.getSqrFt() - h2.getSqrFt());
-	}
-
-	/**
-	 * Takes in all the house specifications
-	 * 
-	 * @param takes
-	 *            in the coordinates of x and y aswell as the age, sqrft and the
-	 *            price of the house
-	 */
-	public void addHouse(int x, int y, String a, int sf, float p) {
-		houses.add(new House(x, y, a, sf, p));
-	}
-
-	/**
-	 * Sets the house specifications
-	 * 
-	 * @param takes
-	 *            in the coordinates of x and y aswell as the age, sqrft and the
-	 *            price of the house
-	 */
-	public void setNewHouse(int x, int y, String a, int sf) {
-		newHouse = new House(x, y, a, sf);
-	}
-
-	/**
-	 * Clears the nearest neighbours.
-	 */
-	public void resetNN() {
-		nearestNeighbors.clear();
-	}
-
-	/**
-	 * Void method that sets the new house price
-	 */
-	public void setNewHousePrice() {
-		newHouse.setPrice(findPrice());
-	}
-
-	/**
-	 * A getter for houses
-	 * 
-	 * @return an arraylist of type house which returns the current house
-	 */
-	public ArrayList<House> getHouses() {
-		return (ArrayList<House>) houses;
-	}
-
-	/**
-	 * A getter for new houses
-	 * 
-	 * @return type of house the new house added.
-	 */
-	public House getNewHouse() {
-		return newHouse;
-	}
-
 }
