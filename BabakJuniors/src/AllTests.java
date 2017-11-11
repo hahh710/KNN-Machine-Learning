337843d1e0db76d95077bc7895a0739846baea2c
Ahmed Khattab <ahmed.khattab20@hotmail.com>
2017-11-11 13:21:49 -0500

    AllTests Update

--- BabakJuniors/src/AllTests.java
+++ BabakJuniors/src/AllTests.java
@@ -5,7 +5,12 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 @SuiteClasses({
 	RankTest.class,
-	FeatureTest.class
+	FeatureTest.class,
+	TestingExampleTest.class,
+	TrainingExampleTest.class,
+	ExampleTest.class,
+	DistanceTest.class,
+	ExampleManagerTest.class
 })
 
 public class AllTests {
