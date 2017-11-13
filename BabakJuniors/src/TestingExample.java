97c635ce712016d6c52fd31b2649617b4e5d2ceb
Arsalan Sadiq <32456066+arsalansadiq@users.noreply.github.com>
2017-11-13 04:51:52 +0400

    added java doc

--- BabakJuniors/src/TestingExample.java
+++ BabakJuniors/src/TestingExample.java
@@ -1,5 +1,24 @@
-
-
+/**
+ * 
+ * @author Arsalan Sadiq
+ * @Purpose: inherits all the features from the training example and utilizes
+ *           the the distance class to predict a certain feature compared to
+ *           Training Example
+ *
+ * @field variables - distance Distance: an instance of distance class to update
+ *        the distances - feature Feature: an instance of feature class to use
+ *        it to add it to the testing example - knn KNN: an instance of KNN
+ *        class to use it to add it to the testing example - k int : put the k
+ *        value into the testing example withing the KNN class - exampleManager
+ *        Example: instance of Example to get the list of the training example
+ *        methods and used for ranking
+ * 
+ * @methods +getDistances: returns the distances +getExample: returns the
+ *          exampleManager getK: returns the k value setK: changes the k value
+ *          predictFeature: Feature :predicts the feature depending on the type
+ *          of the feature and changes the feature in the list of testing
+ *          example
+ */
 public class TestingExample extends TrainingExample {
 
 	private Distance distances;
@@ -12,15 +31,56 @@ public class TestingExample extends TrainingExample {
 		this.setK(k);
 		this.feature = feature;
 		exampleManager = eM;
-		distances=new Distance(this);
+		distances = new Distance(this);
 		knn = new KNN(k, this);
 	}
-	public Distance getDistances(){
+
+	/**
+	 * getDistances
+	 * 
+	 * @return distances
+	 */
+	public Distance getDistances() {
 		return distances;
 	}
-	public Example getExample(){
+
+	/**
+	 * getExample
+	 * 
+	 * @return exampleManager
+	 */
+	public Example getExample() {
 		return exampleManager;
 	}
+
+	/**
+	 * getK
+	 * 
+	 * @return k
+	 */
+	public int getK() {
+		return k;
+	}
+
+	/**
+	 * setK
+	 * 
+	 * @param k
+	 *            set the k value in the knn instance
+	 */
+	public void setK(int k) {
+		// this.k = k;
+		knn.setK(k);
+	}
+
+	/**
+	 * PredictFeature
+	 * 
+	 * @param f
+	 * 
+	 *            sets the feature value passed in the parameter as the average
+	 *            of the list of features in the example class.
+	 */
 	public void PredictFeature(Feature f) {
 		String fName = f.getFName();
 		int count = 0;
@@ -30,15 +90,12 @@ public class TestingExample extends TrainingExample {
 		// Subjective f
 		if (exampleManager.checkSubjective(f)) {
 			int position = 0;
-			for (TrainingExample t :knn.getNN()) {
+			for (TrainingExample t : knn.getNN()) {
 				position += t.getAllFeatures().get(fName).getRank();
 				count++;
 			}
 			position = position / count;
-			//getValueAtRank(position)
-			
 			feature.setStringValue(feature.getRankList().getValueAtRank(position));
-			//
 		}
 		if (exampleManager.checkAbsolute(f)) {
 			int sum = 0;
@@ -65,11 +122,5 @@ public class TestingExample extends TrainingExample {
 		}
 
 	}
-	public int getK() {
-		return k;
-	}
-	public void setK(int k) {
-		this.k = k;
-		knn.setK(k);
-	}
+
 }
