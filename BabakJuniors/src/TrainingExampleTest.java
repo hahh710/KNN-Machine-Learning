88394f62994fd0ba636ef15135a49009f33d2ddf
Ahmed Khattab <ahmed.khattab20@hotmail.com>
2017-11-13 21:15:35 -0500

    update

--- BabakJuniors/src/TrainingExampleTest.java
+++ BabakJuniors/src/TrainingExampleTest.java
@@ -1,24 +1,34 @@
 import static org.junit.Assert.*;
 
+import java.util.HashMap;
+
+import javax.swing.DefaultListModel;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class TrainingExampleTest {
+	private String exampleName;
+	private HashMap<String,Feature> feature; 
+	private DefaultListModel<String>nameSet;
+	@Before
+	public void setUp() throws Exception {
+		exampleName = new String();
+		feature = new HashMap<String,Feature>();
+		nameSet = new DefaultListModel<String>();
+	}
 
-@Before
-public void setUp() throws Exception {
-	
-}
+	@After
+	public void tearDown() throws Exception {
+	}
 
-@After
-public void tearDown() throws Exception {
-}
-
-@Test
-public void testRank() {
-
-}
+	@Test
+	public void testTrainingExample() {
+		assertNotNull("exampleName is not null", exampleName);
+		assertNotNull("feature is not null", feature);
+		assertNotNull("nameSet is not null", nameSet);
+	}
 
 }
 
