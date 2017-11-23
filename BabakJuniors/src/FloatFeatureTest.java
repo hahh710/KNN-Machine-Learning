d658aab79b3c657ce204a3440f7f30fec50b4688
Arsalan Sadiq <32456066+arsalansadiq@users.noreply.github.com>
2017-11-23 15:02:49 -0600

    update

--- BabakJuniors/src/FloatFeatureTest.java
+++ BabakJuniors/src/FloatFeatureTest.java
@@ -1,4 +1,52 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 public class FloatFeatureTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetDistance() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFloatFeature() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAbsoluteDistance() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSquareDistance() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSubValue() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetValue() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetValue() {
+		fail("Not yet implemented");
+	}
 
 }
