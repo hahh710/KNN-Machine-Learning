ccc24cedbcdeef2f3aaa523b8a6c3b3632c6472e
hahh710 <hunhoha@cmail.carleton.ca>
2017-11-24 18:34:32 -0500

    compositeFeature
    composite

---
+++ BabakJuniors/src/CompositeFeature.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+public class CompositeFeature extends Feature {
+	private ArrayList<Feature> subFeatures;
+	private ArrayList<String> subFeatureNames;
+	public CompositeFeature(String s) {
+		super(s);
+		subFeatures = new ArrayList<Feature>();
+		subFeatureNames = new ArrayList<String>();
+	}
+	public void addFeature(Feature feature) {
+		subFeatures.add(feature);
+		subFeatureNames.add(feature.getFName());
+	}
+
+	public ArrayList<Feature> getSubFeatures() {
+		return subFeatures;
+	}
+	public void setSubFeatures(ArrayList<Feature> subFeatures) {
+		this.subFeatures = subFeatures;
+	}
+	public ArrayList<String> getSubFeatureNames() {
+		return subFeatureNames;
+	}
+	public void setSubFeatureNames(ArrayList<String> subFeatureNames) {
+		this.subFeatureNames = subFeatureNames;
+	}
+	
+	@Override
+	public Float getDistance(Feature f, String metric) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
