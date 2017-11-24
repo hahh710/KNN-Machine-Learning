d9e3ac51edccef8f1bfbabe52cfa8a7a17fb099c
Arsalan Sadiq <32456066+arsalansadiq@users.noreply.github.com>
2017-11-24 14:50:01 -0600

    update

--- BabakJuniors/src/FloatFeatureTest.java
+++ BabakJuniors/src/FloatFeatureTest.java
@@ -5,9 +5,20 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class FloatFeatureTest {
+	float v1=10;
+	float v2=15;
+	String s1="new1";
+	String s2="new2";
+	FloatFeature f1;
+	FloatFeature f2;
+	String m1="absoluteDistance";
+	String m2="SquareDistance";
 
 	@Before
 	public void setUp() throws Exception {
+	 f1= new FloatFeature(s1,v1);
+	 f2=new FloatFeature(s2,v2);
+		
 	}
 
 	@After
@@ -16,12 +27,13 @@ public class FloatFeatureTest {
 
 	@Test
 	public void testGetDistance() {
-		fail("Not yet implemented");
+		f1.getDistance(f2, m1);
 	}
 
 	@Test
 	public void testFloatFeature() {
-		fail("Not yet implemented");
+		
+		assertEquals(new FloatFeature(s1,v1),f1);
 	}
 
 	@Test
